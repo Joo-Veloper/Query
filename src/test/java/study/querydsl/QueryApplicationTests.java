3b4ce7d3a2f42c46995be3baae8eb49623a24034
Seung Joo <psjoo3515@gmail.com>
2024-02-29 21:02:51 +0900

    Query DSL Setting

---
+++ src/test/java/study/querydsl/QueryApplicationTests.java
@@ -0,0 +1,37 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QueryApplicationTests {
+    //@PersistenceContext -> JAVA 표준 스펙에 사용
+    @Autowired // spring 최신 -> 똑같은 기능으로 동작함!
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        // query DSL 사용
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+}
