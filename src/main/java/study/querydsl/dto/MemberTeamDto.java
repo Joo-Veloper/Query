a3c750bf31b2e12b5661279b054b0c7e12be6e51
Seung Joo <psjoo3515@gmail.com>
2024-03-07 15:18:14 +0900

    동적 쿼리 - Builder

---
+++ src/main/java/study/querydsl/dto/MemberTeamDto.java
@@ -0,0 +1,23 @@
+package study.querydsl.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+@Data
+public class MemberTeamDto {
+    private Long memberId;
+    private String username;
+    private int age;
+    private Long teamId;
+    private String teamName;
+
+    /**DTO가 가급적 순수했으면 좋겠지만 QueryDSL 라이브러이에 의존하는 단점*/
+    @QueryProjection
+    public MemberTeamDto(Long memberId, String username, int age, Long teamId, String teamName) {
+        this.memberId = memberId;
+        this.username = username;
+        this.age = age;
+        this.teamId = teamId;
+        this.teamName = teamName;
+    }
+}
