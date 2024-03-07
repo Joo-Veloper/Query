683ce0ccc7ab551d8a771f3bebed3fc8b5eb79ba
Seung Joo <psjoo3515@gmail.com>
2024-03-07 14:35:27 +0900

    Pure JPA repository

--- src/main/java/study/querydsl/QueryApplication.java
+++ src/main/java/study/querydsl/QueryApplication.java
@@ -1,7 +1,10 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QueryApplication {
@@ -9,5 +12,9 @@ public class QueryApplication {
     public static void main(String[] args) {
         SpringApplication.run(QueryApplication.class, args);
     }
+//    @Bean
+//    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+//        return new JPAQueryFactory(em);
+//    }
 
 }
